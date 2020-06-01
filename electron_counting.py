3baa7b79215eed5789be4fb0ba2e7ae2cd7b9a15
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-06-01 15:37:41 +0800

    add seprate bond cutoff
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- electron_counting.py
+++ electron_counting.py
@@ -22,6 +22,8 @@ np.random.seed(seed)
 db = connect('./qm9.db')
 
 ucfc_cutoff = {'H': 1.65, 'C':1.86, 'O':1.86, 'F':1.85, 'N':1.83}
+bond_cutoff = {'H': 1.40, 'C':2.20, 'O':2.20, 'F':2.20, 'N':2.0}
+
 vlnc_cnt = {'H': 1, 'C':4, 'O':6, 'F':7, 'N':5}
 full_cnt = {'H': 2, 'C':8, 'O':8, 'F':8, 'N':8}
 
@@ -46,7 +48,7 @@ def check_ucfc_by_electron_counting(atoms, del_idx, ucfc_tag_lst):
             for j in range(i+1, len(atoms)):
                 if i==del_idx or j==del_idx:
                     continue
-                elif atoms.get_distance(i, j) < (ucfc_cutoff[atoms[i].symbol] + ucfc_cutoff[atoms[j].symbol])/2 and \
+                elif atoms.get_distance(i, j) < (bond_cutoff[atoms[i].symbol] + bond_cutoff[atoms[j].symbol])/2 and \
                         electron_cnt[i] < full_cnt[atoms[i].symbol] and \
                         electron_cnt[j] < full_cnt[atoms[j].symbol]:
                     electron_cnt[i] += 1
@@ -66,11 +68,11 @@ def check_ucfc_by_electron_counting(atoms, del_idx, ucfc_tag_lst):
             err_cnt += 1
         else:
             pass
-    if err_cnt != 0:
-        print('-' * 100)
-        print(electron_cnt, ucfc_tag_lst, err_cnt)
-        print(del_idx)
-        view(atoms)
+    # if err_cnt != 0:
+    #     print('-' * 100)
+    #     print(electron_cnt, ucfc_tag_lst, err_cnt)
+    #     print(del_idx)
+    #     view(atoms)
     return err_cnt
 
 if __name__ == '__main__':
